033ef679d511f8ff3e1de6dc178abc5901221e7e
cipa2189-web <cipa2189@gmail.com>
2026-06-15 13:40:08 +0300

    Change page title from 'Telegram Clone Pro' to 'Chat'

--- server.js
+++ server.js
@@ -241,7 +241,7 @@ const HTML_PAGE = `<!DOCTYPE html>
 <head>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
-<title>Telegram Clone Pro</title>
+<title>Chat</title>
 <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
 <script src="/socket.io/socket.io.js"></script>
 <style>
